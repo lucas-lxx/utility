47ca4654cd8b412e49849e108ca3e63f94209ead
Lucas Almeida <lucas.lxx@alu.ufc.br>
2025-01-30 16:21:50 -0300

    update on the NGINX tutorial 47:49 youtube.com/watch?v=9t9Mp0BGnyI&t=620s

---
+++ nginx/mysite/server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  res.send("I am an endpoint");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
